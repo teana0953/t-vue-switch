ab118d5b2a04bb4771c2d8810545c112e6821e9c
Tina.Liang <teana0953@gmail.com>
2020-10-03 22:08:17 +0800

    Update demo display layout

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
     <div id="app">
         <img alt="Vue logo" src="./assets/logo.png" />
         <app-switch v-model="isOn"></app-switch>
+		<span class="margin-top"></span>
+        <div class="result-text">{{ isOn }}</div>
     </div>
 </template>
 
@@ -23,11 +25,33 @@ export default {
 
 <style>
 #app {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
     margin-top: 60px;
+}
+
+img {
+    align-self: center;
+    width: 200px;
+    height: 200px;
+}
+
+.switch {
+    align-self: center;
+}
+
+.result-text {
+	align-self: center;
+	padding: 4px 6px 4px 6px
+}
+
+.margin-top {
+	margin-top: 10px;
 }
 </style>
